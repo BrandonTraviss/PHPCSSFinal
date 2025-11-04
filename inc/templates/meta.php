82e857a959a36e60438044f1f66919e88c123ec1
BrandonTraviss <btraviss1987@gmail.com>
2025-11-04 09:38:22 -0500

    Meta Template Finished
    Finished the Meta Template file

---
+++ inc/templates/meta.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script type="module" src="./src/js/main.js"></script>
+    <link rel="stylesheet" href="./src/css/style.css">
+    <meta name="description" contents="<?php echo htmlspecialchars($pageDescription) ?>">
+    <meta name="robots" contents="noindex,nofollow">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo htmlspecialchars($pageTitle) ?></title>
+</head>
