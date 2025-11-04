029767760610b1af31742ddd96e8c73562e2f4c8
BrandonTraviss <btraviss1987@gmail.com>
2025-11-04 10:44:02 -0500

    Header/Main CTA Partially done (Not Responsive)
    Header is now partially finished / CTA is also finished (not responsive)
    product.php changed to shop.php

---
+++ inc/templates/header.php
@@ -0,0 +1,21 @@
+<header>
+    <nav>
+        <a class="logo-container" href="">
+            <img src="./src/img/logo.png" alt="">
+        </a>
+        <menu class="desktop-menu">
+            <li><a href="">Shop</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Register</a></li>
+        </menu>
+        <menu class="mobile-menu">
+            <li><a href="">Shop</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">Register</a></li>
+        </menu>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $pageTitle = "Homepage";
+    $description = "Welcome to the bookstore. Your one stop shop for new and used books. Check out our winter sale!";
+    include "./inc/templates/meta.php";
+?>
+
+<body>
+    <section class="sale-CTA">
+        <a href="./shop.php">
+            Winter Reading SALE is ON! Visit the Shop
+        </a>
+    </section>
+    <?php include "./inc/templates/header.php"?>
+    <main>
+
+    </main>
+</body>
+<?php
+    include "./inc/templates/footer.php";
+?>
