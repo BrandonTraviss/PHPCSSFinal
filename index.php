a85a1e271526083194955bc438c6b1b752648d07
BrandonTraviss <btraviss1987@gmail.com>
2025-11-04 14:42:12 -0500

    Index Changed / CSS updated
    Changed the hero for index and removed color variables that are not being used

--- index.php
+++ index.php
@@ -15,6 +15,42 @@
                     <p>Discover captivating stories.</p>
                     <p>curated collections all for a reasonable price.</p>
                 </div>
+                <div class="book-container">
+
+                <article class="book-card">
+                        <a href="">
+                        <div class="book-img-container">
+                            <img src="./uploads/lotrfellowship.jpg" alt="">
+                        </div>
+                        <p><cite>J.R.R. Tolkien</cite></p>
+                        <p>Lord of the Rings The Fellowship Of The Ring</p>
+                        <p>$59.99</p>
+                    </a>
+                </article>
+
+                <article class="book-card">
+                        <a href="">
+                        <div class="book-img-container">
+                            <img src="./uploads/lotrtwotowers.jpg" alt="">
+                        </div>
+                        <p><cite>J.R.R. Tolkien</cite></p>
+                        <p>Lord of the Rings The Two Towers</p>
+                        <p>$59.99</p>
+                    </a>
+                </article>
+
+                <article class="book-card">
+                        <a href="">
+                        <div class="book-img-container">
+                            <img src="./uploads/lotrrotk.jpg" alt="">
+                        </div>
+                        <p><cite>J.R.R. Tolkien</cite></p>
+                        <p>Lord of the Rings The Return Of The King</p>
+                        <p>$59.99</p>
+                    </a>
+                </article>
+
+            </div>
                 <a class="btn-brand-2" href="./shop.php">Browse Books</a>
         </section>
     </main>
